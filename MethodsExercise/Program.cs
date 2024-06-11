cd354e1dbba815bf7e22a23b00f69114b9f4f503
Shayn Coffey <Shayncoffey30@gmail.com>
2024-06-11 19:18:50 -0400

    complete exercise part 1

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Madlib();
         }
+
+        public static void Madlib()
+        {
+
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Hello, {userName}! What is your favorite color?");
+            string favcolor = Console.ReadLine();
+            Console.WriteLine("Favorite band?");
+            string favBand = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal?");
+            string favAnimal = Console.ReadLine();
+
+            Console.WriteLine($"One day, {userName} was walking through the woods while wearing a {favcolor} t-shirt. {userName} saw a {favAnimal} listening to {favBand}.");
+            
+        }    
     }
 }
